d4c4ccfa8d4901061702cd81c99383e25ff46014
Su Sung Chung <css_0524@hotmail.com>
2022-03-24 23:45:04 -0400

    setup canvas

--- snake.js
+++ snake.js
@@ -1,2 +1,13 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+class Snake{
+    constructor(){
+        this.x = 0
+        this.y = 0
+    }
+
+}
+
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+ctx.rect(10,10,500,500);
+ctx.fill();
