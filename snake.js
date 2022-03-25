64ede77408ba8c1e0b3a52ae2019e749079d53cf
Su Sung Chung <css_0524@hotmail.com>
2022-03-25 18:53:52 -0400

    create basic snake movement and animation loop

--- snake.js
+++ snake.js
@@ -6,17 +6,66 @@ const GAMEHEIGHT = 10;
 
 class Snake{
     constructor(){
-        this.x = 0
-        this.y = 0
+        this.x = 5;
+        this.y = 5;
+        this.dir = "down";
+        this.speed = 0.3;
     }
 
     draw(){
         ctx.beginPath();
-        ctx.fillStyle = 'white'
-        ctx.strokeStyle = 'transparent'
-        ctx.rect(GAMEX + this.x * SQUARESIZE,GAMEY + this.y * SQUARESIZE,SQUARESIZE,SQUARESIZE)
+        ctx.fillStyle = 'white';
+        ctx.strokeStyle = 'transparent';
+        ctx.rect(GAMEX + this.x * SQUARESIZE,GAMEY + this.y * SQUARESIZE,SQUARESIZE,SQUARESIZE);
         ctx.fill();
         ctx.stroke();
+    }
+
+    move(){
+        let new_x = this.x;
+        let new_y = this.y;
+        switch (this.dir){
+            case 'left':
+                new_x -= this.speed;
+                break;
+            case 'right':
+                new_x += this.speed;
+                break;
+            case 'up':
+                new_y -= this.speed;
+                break;
+            case 'down':
+                new_y += this.speed;
+                break;
+        }
+        if (this.tempCheckColision(new_x,new_y)){
+            // TODO: allow it to move & change game state to gameover
+            
+        }
+        else{
+            this.x = new_x;
+            this.y = new_y;
+        }
+    }
+
+    tempCheckColision(){
+        if (this.x <= 0 || this.x > GAMEX-2){
+            return true;
+        }
+        if (this.y <= 0 || this.y > GAMEY-2){
+            return true;
+        }
+        return false;
+    }
+
+    CheckColision(){
+        if (this.x < 0 || this.x > GAMEX-1){
+            return true;
+        }
+        if (this.y < 0 || this.y > GAMEY-1){
+            return true;
+        }
+        return false;
     }
 }
 
@@ -25,10 +74,20 @@ class Snake{
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'black'
-ctx.strokeStyle = 'black'
-ctx.rect(GAMEX,GAMEY,SQUARESIZE * GAMEWIDTH, SQUARESIZE * GAMEHEIGHT);
-ctx.fill();
 
 snake = new Snake();
-snake.draw();
+
+
+animate()
+function background(){
+    ctx.fillStyle = 'black'
+    ctx.strokeStyle = 'black'
+    ctx.rect(GAMEX,GAMEY,SQUARESIZE * GAMEWIDTH, SQUARESIZE * GAMEHEIGHT);
+    ctx.fill();
+}
+function animate(){
+    window.requestAnimationFrame(animate);
+    background();
+    snake.move();
+    snake.draw();
+}
