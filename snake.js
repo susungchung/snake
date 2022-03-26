87c70446900e702f4c260acfed514c287a7e1a3e
Su Sung Chung <css_0524@hotmail.com>
2022-03-25 20:28:54 -0400

    implement drawing feature that allows drawing multiple blocks with turning points

--- snake.js
+++ snake.js
@@ -8,42 +8,84 @@ class Snake{
     constructor(){
         this.x = 5;
         this.y = 5;
-        this.dir = "down";
+        this.dir = "left";
         this.speed = 1;
+        this.length = 8;
+        this.turnPoints = [];
     }
 
     draw(){
         ctx.beginPath();
         ctx.fillStyle = 'white';
-        ctx.strokeStyle = 'transparent';
-        ctx.rect(GAMEX + this.x * SQUARESIZE,GAMEY + this.y * SQUARESIZE,SQUARESIZE,SQUARESIZE);
-        ctx.fill();
+        //ctx.strokeStyle = 'transparent';
+        let cur_x = this.x;
+        let cur_y = this.y;
+        let cur_dir = this.reverseDir(this.dir);
+        let tp_index = 0;
+        for (var i = 0;i<this.length;i++){
+            ctx.rect(GAMEX + cur_x * SQUARESIZE,GAMEY + cur_y * SQUARESIZE,SQUARESIZE,SQUARESIZE);
+            ctx.fill();
+            let new_cord = this.updateCordinate(cur_x,cur_y,cur_dir,1);
+            cur_x = new_cord.x;
+            cur_y = new_cord.y;
+            if (tp_index < this.turnPoints.length && cur_x == this.turnPoints[tp_index].x && cur_y == this.turnPoints[tp_index].y){
+                console.log(this.turnPoints[tp_index]);
+                cur_dir = this.reverseDir(this.turnPoints[tp_index].dir);
+                tp_index += 1;
+            }
+        }
+        while(tp_index < this.turnPoints.length){
+            this.turnPoints.pop();
+        }
         ctx.stroke();
+    }
+
+    reverseDir(dir){
+        switch (dir){
+            case 'left':
+                return 'right';
+                break;
+            case 'right':
+                return 'left';
+                break;
+            case 'up':
+                return 'down';
+                break;
+            case 'down':
+                return 'up';
+                break;
+                    
+        }
+    }
+    updateCordinate(x,y,dir,delta){
+        switch (dir){
+            case 'left':
+                x -= delta;
+                break;
+            case 'right':
+                x += delta;
+                break;
+            case 'up':
+                y -= delta;
+                break;
+            case 'down':
+                y += delta;
+                break;
+        }
+        return {x,y}
     }
 
     move(){
         //console.log(this.dir,this.x,this.y);
         let new_x = this.x;
         let new_y = this.y;
-        switch (this.dir){
-            case 'left':
-                new_x -= this.speed;
-                break;
-            case 'right':
-                new_x += this.speed;
-                break;
-            case 'up':
-                new_y -= this.speed;
-                break;
-            case 'down':
-                new_y += this.speed;
-                break;
-        }
-        
+        let new_cord = this.updateCordinate(new_x,new_y,this.dir,this.speed);
+        new_x = new_cord.x;
+        new_y = new_cord.y;
+
         // TODO: change it to proper collision detector
         if (this.tempCheckColision(new_x,new_y)){ 
             //TODO: even in case of collision allow it to move and change game state to gameover
-            console.log(new_x,new_y)
         }
         else{
             this.x = new_x;
@@ -69,6 +111,14 @@ class Snake{
             return true;
         }
         return false;
+    }
+
+    logTurn(key){
+        if (this.dir != key && this.dir != this.reverseDir(key)){
+            
+            this.turnPoints.unshift( {x : this.x, y : this.y, dir : this.dir} )
+            this.dir = key
+        }
     }
 }
 
@@ -123,16 +173,16 @@ window.addEventListener('keydown', (e) => {
     console.log(e);
     switch(e.key){
         case 'ArrowRight':
-            player.dir = 'right';
+            player.logTurn ('right');
             break;
         case 'ArrowLeft':
-            player.dir = 'left';
+            player.logTurn('left');
             break;
         case 'ArrowUp':
-            player.dir = 'up';
+            player.logTurn('up');
             break;
         case 'ArrowDown':
-            player.dir = 'down';
+            player.logTurn('down');
             break;
     };
 });
